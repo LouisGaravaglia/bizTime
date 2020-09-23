9c8daffae4538952cbaa1e198074f391719845ab
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-09-22 18:02:23 -0700

    added an association route for business and industry. Going to work on testing next

--- routes/industries.js
+++ routes/industries.js
@@ -4,11 +4,6 @@ const ExpressError = require("../expressError");
 const db = require("../db");
 var slugify = require('slugify')
 
-// Add routes for:
-
-//     adding an industry
-//     associating an industry to a company
-
 router.get("/", async (req, res, next) => {
     try {
          const industryResults = await db.query(`
@@ -19,6 +14,31 @@ router.get("/", async (req, res, next) => {
         GROUP BY i.ind_code
         `)
         return res.json({compResults: industryResults.rows})
+    } catch(e) {
+        return next(e);
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const {name} = req.body;
+        const code = slugify(name, {replacement: '_', remove: /[*+~.()'"!:@]/g, lower: true, strict: true})
+        const result = await db.query(`INSERT INTO industries (ind_code, name) VALUES ($1, $2) RETURNING *`, [code, name]);
+        return res.status(201).json({company: result.rows[0]})
+    } catch(e) {
+        return next(e);
+    }
+})
+
+router.post("/:comp_code", async (req, res, next) => {
+    try {
+        const {ind_code} = req.body;
+        const company = await db.query(`SELECT * FROM companies WHERE code=$1`, [req.params.comp_code]);
+        if(company.rows.length === 0) {
+            throw new ExpressError(`Company not found with the code: ${req.params.comp_code}`, 404)
+        }
+        const result = await db.query(`INSERT INTO company_industry (c_code, i_code) VALUES ($1, $2) RETURNING *`, [req.params.comp_code, ind_code]);
+        return res.status(201).json({company: result.rows[0]})
     } catch(e) {
         return next(e);
     }
